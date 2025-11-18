5ba40accc603b187d14aafaf27e413256520e2ad
SHIVAM PRASAD <shivamprasad012@gmail.com>
2025-11-18 09:26:52 +0530

    updated footer text

--- app/layout.js
+++ app/layout.js
@@ -36,7 +36,7 @@ export default function RootLayout({ children }) {
             {/* footer */}
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made by Shivam 😎</p>
+                <p>Made by Invaders 😎</p>
               </div>
             </footer>
           </ThemeProvider>
